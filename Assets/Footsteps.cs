a4e4f36416c34cbb33806ae03089fd13e4bd9b74
Sascha Niemann <sascha.niemann@r132-10-w10.SCHULE-ISERV>
2018-02-15 14:38:09 +0100

    bis footsteps

---
+++ Assets/Footsteps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+	public AudioClip audioClip;
+	public float stepLength = 0.4F;
+	public float volume = 0.7F;
+	private CharacterController controller;
+	private float delay = 0;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller.velocity.sqrMagnitude > 0.2F && controller.isGrounded) {
+			if (delay >= stepLength) {
+				AudioSource.PlayClipAtPoint (audioClip, transform.position, volume);
+				delay = 0;
+			}
+		}
+		delay += Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float moveSpeed = 5.0F;
+	public float rotationSpeed = 300.0F;
+	public float rotationInterval;
+	private Vector3 moveDirection = Vector3.zero;
+	private CharacterController controller;
+	private Quaternion destRotation;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		destRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection = moveDirection * moveSpeed;
+		controller.SimpleMove (moveDirection);
+
+		if (Input.GetKey (KeyCode.Q)) {
+			destRotation.eulerAngles = destRotation.eulerAngles - new Vector3 (0, rotationInterval, 0);
+		}
+
+		if (Input.GetKey (KeyCode.E)) {
+			destRotation.eulerAngles = destRotation.eulerAngles + new Vector3 (0, rotationInterval, 0);
+		}
+
+		float step = rotationSpeed * Time.deltaTime;
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, destRotation, step);
+	}
+}
